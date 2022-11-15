579cf0fc0b786e310cac7211f28952033876b520
MohamedYDahab <118087657+MohamedYDahab@users.noreply.github.com>
2022-11-15 15:37:59 -0800

    Update inherit_stock_adjustment.py

--- expiry_dates/models/inherit_stock_adjustment.py
+++ expiry_dates/models/inherit_stock_adjustment.py
@@ -5,13 +5,11 @@ from datetime import datetime, date
 class InheritStockAdjustment(models.Model):
     _inherit = 'stock.quant'
 
-    lot_expiray = fields.Date(string='Expiray Date')
+    lot_expiry = fields.Date(string='Expiry Date')
     lot_id = fields.Many2one(
-        'stock.production.lot', 'Lot/Serial Number', index=True,
+        'stock.lot', 'Lot/Serial Number', index=True,
         ondelete='restrict', check_company=True,
         domain=lambda self: self._domain_lot_id(), required=True)
-
-
 
 
 
@@ -19,23 +17,23 @@ class InheritStockAdjustment(models.Model):
     def _get_inventory_fields_create(self):
         """ Returns a list of fields user can edit when he want to create a quant in `inventory_mode`.
         """
-        return ['product_id', 'location_id', 'lot_id', 'package_id', 'owner_id','lot_expiray'] + self._get_inventory_fields_write()
+        return ['product_id', 'location_id', 'lot_id', 'package_id', 'owner_id','lot_expiry'] + self._get_inventory_fields_write()
 
 
-
-
-    @api.onchange('lot_expiray')
+    @api.onchange('lot_expiry')
     def compute_expiry(self):
-        adjust_obj = self.env['stock.production.lot']
-        search_obj = adjust_obj.search([])
-        for obj in search_obj:
-            if self.lot_id.name == obj.name:
-                self.lot_id.expiration_date = self.lot_expiray
+        adjust_obj = self.env['stock.lot']
+        group_obj = adjust_obj.read_group(domain=[],fields=['name'], groupby=['name'])
+
+        for x in range(0,len(group_obj)):
+            if group_obj[x]['name'] == self.lot_id.name:
+                self.lot_id.expiration_date = self.lot_expiry
+
 
 
 
 class StockProductionLotInheritanve(models.Model):
-    _inherit = 'stock.production.lot'
+    _inherit = 'stock.lot'
 
 
     expiration_date = fields.Datetime(string='Expiration Date')
