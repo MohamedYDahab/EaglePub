60eadf8b0902f06750e0d301f3222dabcdd9aa7c
Mohamed Y. Dahab <mohamed.yaseen.dahab@gmail.com>
2026-03-10 01:23:33 +0200

    Negativce Restriction 19

--- negative_stock_restriction/__manifest__.py
+++ negative_stock_restriction/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Stock Restrict Negative Quantity in PoS SO and Stock Move',
-    'version': '19.0.2.0.0',
+    'version': '19.0.1.0.0',
     'summary': 'Prevent negative inventory in Stock, POS, and Sales',
     'description': """
         Comprehensive negative stock restriction module:
